20b93d2b9e61d6fe49f686f1638ae6c00b72ccb1
Jamie Adkins <ja837@york.ac.uk>
2015-07-25 12:54:17 +0100

    DataSourceInfamies cleanup

--- app/src/main/java/com/dawgandpony/pd2skills/Database/DataSourceInfamies.java
+++ app/src/main/java/com/dawgandpony/pd2skills/Database/DataSourceInfamies.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 
 /**
  * Created by Jamie on 25/07/2015.
+ * This class is really used bevasue infamy ids can be calculated. There are only 16 possibilities.
  */
 public class DataSourceInfamies {
     private Context context;
@@ -44,73 +45,11 @@ public class DataSourceInfamies {
 
     }
 
-    public ArrayList<Boolean> createAndInsertInfamy(){
-        ContentValues values = new ContentValues();
-
-
-
-        long id = database.insert(MySQLiteHelper.TABLE_INFAMY, null, values);
-
-        Cursor cursorBuild = database.query(MySQLiteHelper.TABLE_INFAMY,
-                allColumns, MySQLiteHelper.COLUMN_ID + " = " + id, null,
-                null, null, null);
-        cursorBuild.moveToFirst();
 
 
 
 
 
-        ArrayList<Boolean> newInfamy = cursorToInfamy(cursorBuild);
-        cursorBuild.close();
-
-        return newInfamy;
-
-
-
-    }
-
-    public ArrayList<Boolean> getInfamy(long id){
-        ArrayList<Boolean> newInfamy;
-
-        Cursor cursor = database.query(MySQLiteHelper.TABLE_INFAMY,
-                allColumns, MySQLiteHelper.COLUMN_ID + " = " + id, null,
-                null, null, null);
-
-        if (cursor.moveToFirst()) {
-
-            newInfamy = cursorToInfamy(cursor);
-            cursor.close();
-
-
-        } else {
-            cursor.close();
-            newInfamy =  null;
-        }
-
-
-
-        return newInfamy;
-    }
-
-
-
-    private ArrayList<Boolean> cursorToInfamy(Cursor cursor){
-        ArrayList<Boolean> infamy = new ArrayList<>();
-
-        int mastermind = cursor.getInt(1);
-        int enforcer = cursor.getInt(2);
-        int technician = cursor.getInt(3);
-        int ghost = cursor.getInt(4);
-
-        infamy.add(mastermind != 0);
-        infamy.add(enforcer != 0);
-        infamy.add(technician != 0);
-        infamy.add(ghost != 0);
-
-
-
-        return infamy;
-    }
 
     public static ArrayList<Boolean> idToInfamy(long id){
         ArrayList<Boolean> infamy = new ArrayList<>();
