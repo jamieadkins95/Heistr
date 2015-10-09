ca3b9af2df282934cd00df77290cc2eb3caa89ca
Jamie Adkins <ja837@york.ac.uk>
2015-10-09 20:03:45 +0100

    v1.2.2

--- app/src/main/java/com/dawgandpony/pd2skills/Activities/EditBuildActivity.java
+++ app/src/main/java/com/dawgandpony/pd2skills/Activities/EditBuildActivity.java
@@ -86,12 +86,10 @@ public class EditBuildActivity extends MaterialNavigationDrawer implements TaskF
 
 
 
-
         setDrawerHeaderImage(R.drawable.payday_2_logo);
 
 
         MaterialSection secInfamy = newSection("Infamy", InfamyFragment.newInstance());
-
 
         MaterialSection secMas = newSection("Mastermind", SkillTreeFragment.newInstance(Trees.MASTERMIND));
         MaterialSection secEnf = newSection("Enforcer", SkillTreeFragment.newInstance(Trees.ENFORCER));
