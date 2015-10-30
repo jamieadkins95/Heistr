cb4b8045c23aff7450a134449f893f428ec1faed
Jamie Adkins <ja837@york.ac.uk>
2015-10-30 23:15:24 +0000

    v1.3.0.1

--- app/src/main/java/com/dawgandpony/pd2skills/Database/MySQLiteHelper.java
+++ app/src/main/java/com/dawgandpony/pd2skills/Database/MySQLiteHelper.java
@@ -14,7 +14,7 @@ import com.dawgandpony.pd2skills.BuildObjects.Attachment;
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "pd2skills.db";
-    private static final int DATABASE_VERSION = 21;
+    private static final int DATABASE_VERSION = 22;
 
     //region Skills
     public static final String TABLE_SKILL_BUILDS = "tbSkillBuilds";
