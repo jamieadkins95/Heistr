1b092f3461e92f32effecf4e967fc2785493ab40
Jamie Adkins <ja837@york.ac.uk>
2015-12-23 19:22:15 +0000

    Testing GCM

--- app/src/main/java/com/dawgandpony/pd2skills/Activities/BuildListActivity.java
+++ app/src/main/java/com/dawgandpony/pd2skills/Activities/BuildListActivity.java
@@ -1,16 +1,24 @@
 package com.dawgandpony.pd2skills.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.dawgandpony.pd2skills.Fragments.BuildListFragment;
+import com.dawgandpony.pd2skills.Online.RegistrationIntentService;
 import com.dawgandpony.pd2skills.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 
 public class BuildListActivity extends AppCompatActivity {
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "BuildListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,11 @@ public class BuildListActivity extends AppCompatActivity {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new BuildListFragment())
                     .commit();
+        }
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
         }
     }
 
@@ -41,5 +54,21 @@ public class BuildListActivity extends AppCompatActivity {
             Toast.makeText(this, "Coming Soon!", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
