bba2daa86f19d8073d98462afcae304114e1fe89
Jamie Adkins <ja837@york.ac.uk>
2015-12-24 00:01:06 +0000

    obfuscate spi keys

---
+++ app/src/main/java/com/dawgandpony/pd2skills/Online/API.java
@@ -0,0 +1,8 @@
+package com.dawgandpony.pd2skills.Online;
+
+/**
+ * Created by Jamie on 23/12/2015.
+ */
+public class API {
+    public static final String SENDER_ID = "get your own!";
+}
--- app/src/main/java/com/dawgandpony/pd2skills/Online/RegistrationIntentService.java
+++ app/src/main/java/com/dawgandpony/pd2skills/Online/RegistrationIntentService.java
@@ -63,7 +63,7 @@ public class RegistrationIntentService extends IntentService {
             // See https://developers.google.com/cloud-messaging/android/start for details on this file.
             // [START get_token]
             InstanceID instanceID = InstanceID.getInstance(this);
-            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+            String token = instanceID.getToken(API.SENDER_ID,
                     GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
             // [END get_token]
             Log.i(TAG, "GCM Registration Token: " + token);
